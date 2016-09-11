c09db24e1799da11e9eca935f2167808dbe4d1cd
Tyler Mitchell <tjm375@nau.edu>
2016-09-10 22:23:47 -0700

    syntax fixing

--- ppmrw.c
+++ ppmrw.c
@@ -1,39 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int p3top3(FILE input, FILE output);
-int p3top6(FILE input, FILE output);
-int p6top6(FILE input, FILE output);
-int p6top3(FILE input, FILE output);
+//int p3top3(FILE input, FILE output);
+//int p3top6(FILE input, FILE output);
+//int p6top6(FILE input, FILE output);
+//int p6top3(FILE input, FILE output);
 
 typedef struct RGBPixelpack{
   unsigned char R, G, B;
 }
 
-int p3top3(FILE input, FILE output){
+RGBPixelpack;
 
-}
+//int p3top3(FILE input, FILE output){}
 
-int p3top6(FILE input, FILE output){
+//int p3top6(FILE input, FILE output){}
 
-}
+//int p6top6(FILE input, FILE output){}
 
-int p6top6(FILE input, FILE output){
-
-}
-
-int p6top3(FILE input, FILE output){
-
-}
+//int p6top3(FILE input, FILE output){}
 
 int main(int argc, char *argv[]){
-  if [argc != 3] {
+  if (argc != 3) {
     printf("usage: magicnumber input.ppm output.ppm\n");
     return(1);
   }
   FILE *inputfile = fopen(argv[2], "r");
-  FILE *outputfile = fopen(arv[3], "w");
+  FILE *outputfile = fopen(argv[3], "w");
   char c = fgetc(inputfile);
+  int i = atoi (argv[1]);
   if (c != 'P'){
     fprintf(stderr, "Error: Not a supported file format\n");
     return(1);
@@ -43,19 +38,19 @@ int main(int argc, char *argv[]){
     fprintf(stderr, "Error: Not a supported file format\n");
     return(1);
   }
-  if (d == '3' && argv[1] == '3'){
+  if (d == '3' && i == '3'){
     p3top3(inputfile, outputfile);
   }
-  else if (d == '3' && argv[1] ==' 6'){
+  else if (d == '3' && i == '6'){
     p3top6(inputfile, outputfile);
   }
-  else if (d == '6' && argv[1] == '6'){
+  else if (d == '6' && i == '6'){
     p6top6(inputfile, outputfile);
   }
-  else if (d == '6' && argv[1] == '3'){
+  else if (d == '6' && i == '3'){
     p6top3(inputfile, outputfile);
   }
-  int fclose(argv[3]);
-  int fclose(argv[2]);
+  fclose(outputfile);
+  fclose(inputfile);
   return(0);
 }
