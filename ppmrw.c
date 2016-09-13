8914d3eb826c9020bb7e61c53be1f10498aeae74
Tyler Mitchell <tjm375@nau.edu>
2016-09-12 23:41:40 -0700

    added header writing to outfile

--- ppmrw.c
+++ ppmrw.c
@@ -2,18 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 
-//int p3top3(FILE input, int width, int height, FILE output);
-//int p3top6(FILE input, int width, int height, FILE output);
-//int p6top6(FILE input, int width, int height, FILE output);
-//int p6top3(FILE input, int width, int height, FILE output);
+int p3top3(FILE input, int width, int height, FILE output);
+int p3top6(FILE input, int width, int height, FILE output);
+int p6top6(FILE input, int width, int height, FILE output);
+int p6top3(FILE input, int width, int height, FILE output);
 
 typedef struct RGBPixelpack{
   unsigned char R, G, B;
 }
 
+//fread for bits
+//for loops widthxheight for ascii
+
 RGBPixelpack;
 
-//int p3top3(FILE input, int width, int height, FILE output){}
+int p3top3(FILE input, int width, int height, FILE output){
+
+  pixelbuffer = malloc(sizeof(RGBPixelpack)*width*height);
+  rewind(input);
+  char headerdatabuffer[0x1000];
+  fgets(headerdatabuffer, sizeof(headerdatabuffer), input);
+  char e;
+  while(e=fgetc(input)){
+    if (e == '#' || e == '\n'){
+      fgets(headerdatabuffer, sizeof(headerdatabuffer), input);
+    }
+    else if(e != '#')
+      break;
+  }
+  ungetc(e, inputfile);
+  fgets(headerdatabuffer, sizeof(headerdatabuffer), input);
+  fgets(headerdatabuffer, sizeof(headerdatabuffer), input);
+
+
+}
 
 //int p3top6(FILE input, int width, int height, FILE output){}
 
@@ -40,12 +62,21 @@ int main(int argc, char *argv[]){
     return(1);
   }
 
+  if (i == "3"){
+    fprintf(outputfile, "%s", "P3");
+    }
+  }
+
+  else if (i == "6"){
+    fprintf(outputfile, "%s", "P6");
+  }
   rewind(inputfile);
   char throwawaybuff[0x1000];
   fgets(throwawaybuff, sizeof(throwawaybuff), inputfile);
   char e;
   while(e=fgetc(inputfile)){
     if (e == '#' || e == '\n'){
+      ungetc(e, inputfile);
       fgets(throwawaybuff, sizeof(throwawaybuff), inputfile);
     }
     else if(e != '#')
@@ -54,6 +85,7 @@ int main(int argc, char *argv[]){
   char widthxheightbuff[0x1000];
   ungetc(e, inputfile);
   fgets(widthxheightbuff, sizeof(widthxheightbuff), inputfile);
+  fprintf(outprint, "%s", widthxheightbuff);
   char widtharr[0x100];
   char heightarr[0x100];
 
@@ -63,15 +95,16 @@ int main(int argc, char *argv[]){
   int width;
   int height;
 
-  sscanf(widtharr, "%d", &width);
-  sscanf(heightarr, "%d", &height);
+  sscanf(widtharr, "%i", &width);
+  sscanf(heightarr, "%i", &height);
 
 
   char maxvalbuff[0x1000];
   fgets(maxvalbuff, sizeof(maxvalbuff), inputfile);
+  fprintf(outputfile, "%s", maxvalbuff);
 
   int maxval;
-  sscanf(maxvalbuff, "%d", &maxval);
+  sscanf(maxvalbuff, "%i", &maxval);
 
   if (maxval > 255 || maxval < 128){
     fprintf(stderr, "Error: Max color value incorrect");
